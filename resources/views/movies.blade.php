9e104b3ead21018dd8e5f6ae9943f875e029fc7b
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-27 18:41:34 +0200

    add more style to /movies and some footer mod

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,35 +2,39 @@
 
 
 @section('main')
-    <main id="movies_main">
+    <main id="movies_main" class="bg-secondary">
 
         <h1 class="py-5 text-center fw-bold">
             Movies
         </h1>
 
         <div class="container">
-            <div class="row py-5 g-5 row-cols-3">
+            <div class="row border-top border-info pb-5 g-5">
 
+                {{-- dinamic generation of movies col and cards --}}
                 @foreach ($movies as $movie)
-                    <div class="col-3">
-                        <div class="card">
-                            <div class="card-header">
-                                <h6>
+                    <div class="col-4">
+                        <div class="card border-0 bg-dark text-light h-100">
+                            <div class="card-header py-3">
+                                <h5 class="text-danger">
                                     {{ $movie->title }}
-                                </h6>
+                                </h5>
                             </div>
 
-                            <img src="https://picsum.photos/200/200" alt="" class="border">
+                            <img src="https://picsum.photos/200/200" alt="" class="">
 
-                            <div class="card-body">
-                                <p class="">
-                                    {{ $movie->original_title }}, {{ $movie->nationality }}
+                            <div class="card-body d-flex flex-column">
+                                <p class="text-danger fw-bold">
+                                    <span class="text-info fw-bold">Original title: </span>{{ $movie->original_title }}
                                 </p>
-                                <p>
-                                    {{ $movie->date }}
+                                <p class="text-danger fw-bold">
+                                    <span class="text-info fw-bold">Nationality:</span> {{ $movie->nationality }}
                                 </p>
-                                <p>
-                                    {{ $movie->vote }}
+                                <p class="text-danger fw-bold">
+                                    <span class="text-info fw-bold">Release:</span> {{ $movie->date }}
+                                </p>
+                                <p class="text-danger fw-bold">
+                                    <span class="text-info fw-bold">Vote:</span> {{ $movie->vote }}
                                 </p>
                             </div>
                         </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,30 +1,51 @@
 <footer class="bg-dark py-4">
 
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-between">
+
+            {{-- left footer --}}
             <div class="col-3">
-                <h4 class="text-light">
-                    Contacts
-                </h4>
-                <ul class="text-info list-unstyled">
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                </ul>
+                <div class="row">
+                    <div class="col-6">
+                        <h4 class="text-light">
+                            Contacts
+                        </h4>
+                        <ul class="text-info list-unstyled">
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <h4 class="text-light">
+                            About us
+                        </h4>
+                        <ul class="text-info list-unstyled">
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+
+            {{-- right footer --}}
             <div class="col-3">
-                <h4 class="text-light">
-                    About us
-                </h4>
-                <ul class="text-info list-unstyled">
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                    <li><a href="#">Something</a></li>
-                </ul>
+                <div class="row justify-content-end">
+                    <div class="col-2">
+                        <h4 class="text-light">
+                            Contacts
+                        </h4>
+                        <ul class="text-info list-unstyled">
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                            <li><a href="#">Something</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
